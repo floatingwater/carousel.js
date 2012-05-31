ad20374dad6fbe4d0474d0d182ac22c7f7a34cc5
Sabov <alexsabov@gmail.com>
2012-05-31 23:25:07 +0300

    add jquery to project

--- carousel.js
+++ carousel.js
@@ -1,15 +1,24 @@
-console.log(Carousel);
-
 var Carousel = (function(){
-	var Carousel = function(){
-		if(this instanceof Carousel)	{
-			console.log('yes');
-		}
+	var Carousel = function(holder){
+		this.holder = jQuery(holder);
+		this.width  = this.holder.innerWidth();
+		this._initButtons();
 	}
 	Carousel.prototype = {
 		_initButtons: function(){
+			var prev = this.holder.find('.b-carousel__button_action_prev');
+			console.log(this.holder);
+			prev.bind('click', this._prev.bind(this));
+		},
+		_next: function(){
+			
+		},
+		_prev: function(){
 			
 		},
 	}
-	return new Carousel();
+	return Carousel;
 })();
+jQuery(function(){
+	new Carousel('.b-carousel');
+});
