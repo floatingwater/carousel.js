f4c9daa2e4524c5b933654c8f027934f90d0f200
Sabov <alexsabov@gmail.com>
2012-06-02 21:36:07 +0300

    add animation queue for items

--- carousel.js
+++ carousel.js
@@ -1,14 +1,16 @@
 log = console.log;
 
 var Carousel = function(holder, items){
-	this.holder = jQuery(holder);
-	this.items  = jQuery(items);
+	this.holder    = jQuery(holder);
+	this.items     = jQuery(items);
+	this.viewItems = [];
 	this._initBlocks();
 	this._initButtons();
 };
 Carousel.prototype = {
 	//constants
-	ANIMATE_TIME: 1000,
+	SHIFT: 1,
+	ANIMATE_TIME: 500,
 	TEMPLATES: {
 		itemsHolder: '<div class="b-carousel__items-holder"></div>',
 		prevButt: '<button class="b-carousel__button '+
@@ -25,31 +27,29 @@ Carousel.prototype = {
 		           .append(this.itemsHolder)
 		           .append(this.nextButt);
 		this._setBlocks();
-		
 	},
 	_setBlocks: function(){
-		var item       = this.items[0];
+		var item       = this.items[this.items.length - 1];
 		this.itemsHolder.append(item);
+		this.viewItems.push(item.get(0));
 		var width      = item.width();
 		var marginL    = parseInt(item.css('marginLeft'));
 		var marginR    = parseInt(item.css('marginRight'));
 		var outerWidth = width + marginL + marginR;
-		log([outerWidth, width, marginL, marginR]);
 		var itHolWidth = this.itemsHolder.width();
 		this.length    = Math.floor(itHolWidth/outerWidth);
 		var blocks     = outerWidth * this.length - marginR;//last block margin-right
-		log('hol-wid = '+itHolWidth);
-		log('b-w = '+blocks);
 		var margin     = Math.round((itHolWidth-blocks)/(this.length-1));
-		log('margin = '+ margin);
-		item.css('left',  '0px');
-		for(var i = 1; i < this.length; i++){
+		this.viewShift     = outerWidth + margin;
+		item.css('left',  '-' + this.viewShift + 'px');
+		for(var i = 0; i <= this.length; i++){
 			var el   = this.items[i];
-			var left = i*(outerWidth + margin) ;
-			log(left);
+			var left = i*this.viewShift ;
 			el.css('left', left + 'px');
 			this.itemsHolder.append(el);
+			this.viewItems.push(el.get(0));
 		}
+		this.viewItems = jQuery(this.viewItems);
 	},
 	_initButtons: function(){
 		var prev = this.holder.find('.b-carousel__button_action_prev');
@@ -58,16 +58,28 @@ Carousel.prototype = {
 		next.bind('click', this._next.bind(this));
 	},
 	_next: function(){
-		this._move(1);
-	},
-	_prev: function(){
 		this._move(-1);
 	},
+	_prev: function(){
+		this._move(1);
+	},
 	_move: function(shift){
-		this.items.animate({
-			left: (shift < 0 ? '-=' : '+=') + Math.abs(this.itemWidth*shift),
-		}, this.ANIMATE_TIME);
-	}
+		this.viewItems.animate({
+			left: (shift < 0 ? '-=' : '+=') + Math.abs(this.viewShift*this.SHIFT),
+		}, this.ANIMATE_TIME, this._changeItems.bind(this, shift));
+		log('now');
+	},
+	_changeItems: (function(){
+		var i = 0;
+		return function(shift){
+			i++;
+			log(i);
+			if(i === this.viewItems.length){
+				i = 0;
+				
+			}
+		};
+	})()
 };
 
 (function(){
