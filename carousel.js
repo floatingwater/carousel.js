dd6bb3f157cc28b2e34c957ffaae4a59f6122c78
Sabov <alexsabov@gmail.com>
2012-06-03 23:56:27 +0300

    refactor code and fix all bugs

--- carousel.js
+++ carousel.js
@@ -30,26 +30,25 @@ Carousel.prototype = {
 	_setBlocks: function(){
 		var item       = this.items[this.items.length - 1];
 		this.itemsHolder.append(item);
-		this.viewItems.push(item);
+		this.viewItems.push(item.get(0));
 		this.firstI    = this.items.length - 1;
 		var width      = item.width();
-		var marginL    = parseInt(item.css('marginLeft'));
 		var marginR    = parseInt(item.css('marginRight'));
-		var outerWidth = width + marginL + marginR;
+		var outerWidth = item.outerWidth(true);
 		var itHolWidth = this.itemsHolder.width();
 		this.length    = Math.floor(itHolWidth/outerWidth);
 		var blocks     = outerWidth * this.length - marginR;//last block margin-right
 		var margin     = Math.round((itHolWidth-blocks)/(this.length-1));
-		this.viewShift     = outerWidth + margin;
+		this.viewShift = outerWidth + margin;
 		item.css('left',  '-' + this.viewShift + 'px');
 		for(var i = 0; i <= this.length; i++){
 			var el   = this.items[i];
-			var left = i*this.viewShift ;
-			el.css('left', left + 'px');
+			el.css('left', i*this.viewShift + 'px');
 			this.itemsHolder.append(el);
-			this.viewItems.push(el);
+			this.viewItems.push(el.get(0));
 		}
 		this.lastI     = this.length;
+		this.viewItems = jQuery(this.viewItems);
 	},
 	_initButtons: function(){
 		var prev = this.holder.find('.b-carousel__button_action_prev');
@@ -80,7 +79,7 @@ Carousel.prototype = {
 		}
 	})(),
 	_move: function(shift, callback){
-		this.itemsHolder.find('.b-carousel__item').animate({
+		this.viewItems.animate({
 			left: (shift < 0 ? '-=' : '+=') + Math.abs(this.viewShift*this.SHIFT),
 		}, this.ANIMATE_TIME, this._changeItems.bind(this, shift, callback));
 	},
@@ -91,31 +90,29 @@ Carousel.prototype = {
 			if(i === this.viewItems.length){
 				i = 0;
 				if(shift < 0){
-					this.viewItems[0].remove();
+					this.viewItems.first().remove();
 					this.viewItems.splice(0, 1);
-					var last = this.viewItems.length - 1;
-					var item   = this.viewItems[last];
-					this.lastI = this.lastI == (this.items.length - 1) ? 0 : ++this.lastI;
-					var newIt  = this.items[this.lastI];
-					var left   = parseInt(item.css('left'));
+					this.firstI = this.firstI == (this.items.length - 1) ? 0 : ++this.firstI;
+					this.lastI  = this.lastI == (this.items.length - 1) ? 0 : ++this.lastI;
+					var item    = this.viewItems.last();
+					var newIt   = this.items[this.lastI];
+					var left    = parseInt(item.css('left'));
 					newIt.css('left', left + this.viewShift + 'px');
 					this.itemsHolder.append(newIt);
-					this.viewItems.push(newIt);
+					this.viewItems.push(newIt.get(0));
 				}else{
-					var last = this.viewItems.length - 1;
-					this.viewItems[last].remove();
-					this.viewItems.splice(last, 1);
-					var item    = this.viewItems[0];
+					this.viewItems.last().remove();
+					this.viewItems.splice(this.viewItems.length - 1, 1);
 					this.firstI = this.firstI === 0 ? this.items.length - 1 : --this.firstI;
+					this.lastI  = this.lastI === 0 ? this.items.length - 1 : --this.lastI;
+					var item    = this.viewItems.first();
 					var newIt   = this.items[this.firstI];
 					var left    = parseInt(item.css('left'));
 					newIt.css('left', left - this.viewShift + 'px');
 					this.itemsHolder.prepend(newIt);
 					//jQuery colections dosen't support unshift method
-					//Array.prototype.unshift.call(this.viewItems, newIt);
-					this.viewItems.unshift(newIt);
+					Array.prototype.unshift.call(this.viewItems, newIt.get(0));
 				}
-				log(this.viewItems);
 				if(callback) callback();
 			}
 		};
