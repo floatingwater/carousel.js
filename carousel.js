93f9dc82f7c909766a5054268a25055b666f8b57
Sabov <alexsabov@gmail.com>
2012-06-03 16:32:48 +0300

    cant fix bug with scrolling carousel

--- carousel.js
+++ carousel.js
@@ -1,5 +1,4 @@
 log = console.log;
-
 var Carousel = function(holder, items){
 	this.holder    = jQuery(holder);
 	this.items     = jQuery(items);
@@ -31,7 +30,7 @@ Carousel.prototype = {
 	_setBlocks: function(){
 		var item       = this.items[this.items.length - 1];
 		this.itemsHolder.append(item);
-		this.viewItems.push(item.get(0));
+		this.viewItems.push(item);
 		this.firstI    = this.items.length - 1;
 		var width      = item.width();
 		var marginL    = parseInt(item.css('marginLeft'));
@@ -48,39 +47,40 @@ Carousel.prototype = {
 			var left = i*this.viewShift ;
 			el.css('left', left + 'px');
 			this.itemsHolder.append(el);
-			this.viewItems.push(el.get(0));
+			this.viewItems.push(el);
 		}
 		this.lastI     = this.length;
-		this.viewItems = jQuery(this.viewItems);//additional array for animation
 	},
 	_initButtons: function(){
 		var prev = this.holder.find('.b-carousel__button_action_prev');
 		var next = this.holder.find('.b-carousel__button_action_next');
-		prev.bind('click', this._queue.bind(this, this.SHIFT));
-		next.bind('click', this._queue.bind(this, -this.SHIFT));
+		prev.bind('click', this._queue.bind(null, this._move.bind(this), this.SHIFT));
+		next.bind('click', this._queue.bind(null, this._move.bind(this), -this.SHIFT));
 	},
 	_queue: (function(){
-		var clicks  = [];
+		var queueAr  = [];
 		var flag    = false;
 		var dequeue = function(){
-			clicks.splice(0,1);
-			if(clicks.length === 0){
+			queueAr.splice(0,1);
+			if(queueAr.length === 0){
 				flag = false;
 			}else{
-				this._move(clicks[0], dequeue.bind(this));
+				queueAr[0].func(queueAr[0].args, dequeue);
 			}
 		};
-		return function(shift){
+		return function(func, args){
 			if(!flag){
-				log(this);
-				this._move(shift, dequeue.bind(this));
+				func(args, dequeue);
 				flag = true;
 			}
-			clicks.push(shift);
+			queueAr.push({
+				func: func,
+				args: args
+			});
 		}
 	})(),
 	_move: function(shift, callback){
-		this.viewItems.animate({
+		this.itemsHolder.find('.b-carousel__item').animate({
 			left: (shift < 0 ? '-=' : '+=') + Math.abs(this.viewShift*this.SHIFT),
 		}, this.ANIMATE_TIME, this._changeItems.bind(this, shift, callback));
 	},
@@ -91,32 +91,36 @@ Carousel.prototype = {
 			if(i === this.viewItems.length){
 				i = 0;
 				if(shift < 0){
-					this.viewItems.first().remove();
+					this.viewItems[0].remove();
 					this.viewItems.splice(0, 1);
-					var item   = this.viewItems.last();
+					var last = this.viewItems.length - 1;
+					var item   = this.viewItems[last];
 					this.lastI = this.lastI == (this.items.length - 1) ? 0 : ++this.lastI;
 					var newIt  = this.items[this.lastI];
 					var left   = parseInt(item.css('left'));
 					newIt.css('left', left + this.viewShift + 'px');
 					this.itemsHolder.append(newIt);
-					this.viewItems.push(newIt.get(0));
+					this.viewItems.push(newIt);
 				}else{
-					this.viewItems.last().remove();
-					this.viewItems.splice(this.viewItems.length - 1, 1);
-					var item   = this.viewItems.first();
+					var last = this.viewItems.length - 1;
+					this.viewItems[last].remove();
+					this.viewItems.splice(last, 1);
+					var item    = this.viewItems[0];
 					this.firstI = this.firstI === 0 ? this.items.length - 1 : --this.firstI;
-					var newIt  = this.items[this.firstI];
-					var left   = parseInt(item.css('left'));
+					var newIt   = this.items[this.firstI];
+					var left    = parseInt(item.css('left'));
 					newIt.css('left', left - this.viewShift + 'px');
 					this.itemsHolder.prepend(newIt);
-					this.viewItems.push(newIt.get(0));
+					//jQuery colections dosen't support unshift method
+					//Array.prototype.unshift.call(this.viewItems, newIt);
+					this.viewItems.unshift(newIt);
 				}
+				log(this.viewItems);
 				if(callback) callback();
 			}
 		};
 	})()
 };
-
 (function(){
 	var App = function(){
 		var items = this._getGallery();
