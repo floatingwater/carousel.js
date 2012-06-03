34869cd92d398d1afb164ca347c1b5d9967adacb
Sabov <alexsabov@gmail.com>
2012-06-03 14:04:48 +0300

    add simple queue to animation

--- carousel.js
+++ carousel.js
@@ -32,6 +32,7 @@ Carousel.prototype = {
 		var item       = this.items[this.items.length - 1];
 		this.itemsHolder.append(item);
 		this.viewItems.push(item.get(0));
+		this.firstI    = this.items.length - 1;
 		var width      = item.width();
 		var marginL    = parseInt(item.css('marginLeft'));
 		var marginR    = parseInt(item.css('marginRight'));
@@ -49,34 +50,68 @@ Carousel.prototype = {
 			this.itemsHolder.append(el);
 			this.viewItems.push(el.get(0));
 		}
-		this.viewItems = jQuery(this.viewItems);
+		this.lastI     = this.length;
+		this.viewItems = jQuery(this.viewItems);//additional array for animation
 	},
 	_initButtons: function(){
 		var prev = this.holder.find('.b-carousel__button_action_prev');
 		var next = this.holder.find('.b-carousel__button_action_next');
-		prev.bind('click', this._prev.bind(this));
-		next.bind('click', this._next.bind(this));
+		prev.bind('click', this._queue.bind(this, this.SHIFT));
+		next.bind('click', this._queue.bind(this, -this.SHIFT));
 	},
-	_next: function(){
-		this._move(-1);
-	},
-	_prev: function(){
-		this._move(1);
-	},
-	_move: function(shift){
+	_queue: (function(){
+		var clicks  = [];
+		var flag    = false;
+		var dequeue = function(){
+			clicks.splice(0,1);
+			if(clicks.length === 0){
+				flag = false;
+			}else{
+				this._move(clicks[0], dequeue.bind(this));
+			}
+		};
+		return function(shift){
+			if(!flag){
+				log(this);
+				this._move(shift, dequeue.bind(this));
+				flag = true;
+			}
+			clicks.push(shift);
+		}
+	})(),
+	_move: function(shift, callback){
 		this.viewItems.animate({
 			left: (shift < 0 ? '-=' : '+=') + Math.abs(this.viewShift*this.SHIFT),
-		}, this.ANIMATE_TIME, this._changeItems.bind(this, shift));
-		log('now');
+		}, this.ANIMATE_TIME, this._changeItems.bind(this, shift, callback));
 	},
 	_changeItems: (function(){
 		var i = 0;
-		return function(shift){
+		return function(shift, callback){
 			i++;
-			log(i);
 			if(i === this.viewItems.length){
 				i = 0;
-				
+				if(shift < 0){
+					this.viewItems.first().remove();
+					this.viewItems.splice(0, 1);
+					var item   = this.viewItems.last();
+					this.lastI = this.lastI == (this.items.length - 1) ? 0 : ++this.lastI;
+					var newIt  = this.items[this.lastI];
+					var left   = parseInt(item.css('left'));
+					newIt.css('left', left + this.viewShift + 'px');
+					this.itemsHolder.append(newIt);
+					this.viewItems.push(newIt.get(0));
+				}else{
+					this.viewItems.last().remove();
+					this.viewItems.splice(this.viewItems.length - 1, 1);
+					var item   = this.viewItems.first();
+					this.firstI = this.firstI === 0 ? this.items.length - 1 : --this.firstI;
+					var newIt  = this.items[this.firstI];
+					var left   = parseInt(item.css('left'));
+					newIt.css('left', left - this.viewShift + 'px');
+					this.itemsHolder.prepend(newIt);
+					this.viewItems.push(newIt.get(0));
+				}
+				if(callback) callback();
 			}
 		};
 	})()
@@ -90,7 +125,7 @@ Carousel.prototype = {
 	App.prototype = {
 		_getGallery: function(){
 			var items = new Array();
-			for(var i = 0; i < 7; i++){
+			for(var i = 0; i < 10; i++){
 				var div = jQuery('<div class="b-carousel__item"></div>');
 				div.css('background-image', 'url(img/'+i+'.jpg)');
 				items.push(div);
